9ea44cdb3ad63f5d8abb657b20b3f2ff2fae13be
jfqd <jfqd@blun.org>
2019-03-15 12:54:53 +0100

    add my.cnf for proxysql-admin

--- copy/opt/qutic/var/mdata-setup/includes/55-proxysql.sh
+++ copy/opt/qutic/var/mdata-setup/includes/55-proxysql.sh
@@ -8,6 +8,15 @@ fi
 if mdata-get proxysql_admin_pwd 1>/dev/null 2>&1; then
   PROXY_ADMIN_PWD=`mdata-get proxysql_admin_pwd`
   sed -i "s#admin_credentials=\"admin:admin\"#admin_credentials=\"admin:${PROXY_ADMIN_PWD}\"#" /opt/local/etc/proxysql.cnf
+  cat >> /root/.my.cnf << EOF
+[client]
+host = localhost
+port = 3307
+user = admin
+password = ${PROXY_ADMIN_PWD}
+prompt = 'Admin> '
+EOF
+chmod 0400 /root/.my.cnf
 fi
 
 if mdata-get proxysql_database_user 1>/dev/null 2>&1; then
