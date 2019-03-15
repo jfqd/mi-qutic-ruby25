5a487a6164b675e773be2caaffedbf610626c1c8
jfqd <jfqd@blun.org>
2019-03-15 08:13:13 +0100

    move to a here-doc

--- copy/var/zoneinit/includes/18-cronjobs.sh
+++ copy/var/zoneinit/includes/18-cronjobs.sh
@@ -1,8 +1,10 @@
 # add passenger related cronjobs
-CRON='0 3 * * * rm /var/tmp/buffer.* 2>/dev/null || true
-0,10,20,30,40,50 * * * * if [[ $(/opt/qutic/bin/psof --count | /usr/bin/awk "{print $1}") -gt 5000 ]]; then /usr/sbin/svcadm restart apache; fi
+
+cat >> /var/spool/cron/crontabs/root << EOF
+# additions for mi-qutic-ruby25 image
+0 3 * * * rm /var/tmp/buffer.* 2>/dev/null || true
+0,10,20,30,40,50 * * * * if [[ $(/opt/qutic/bin/psof -count-numbers-only) -gt 5000 ]]; then /usr/sbin/svcadm restart apache; fi
 0,10,20,30,40,50 * * * * /usr/bin/mv /tmp/passenger-error* /var/tmp 2>/dev/null || true
 0 4 * * * /usr/bin/find /var/tmp/ -mmin +1440 | /opt/local/bin/grep passenger-error | /usr/bin/xargs /usr/bin/rm
 15 3  * * 0 /usr/sbin/svcadm restart apache
-'
-(crontab -l 2>/dev/null || true; echo "$CRON" ) | sort | uniq | crontab
+EOF
