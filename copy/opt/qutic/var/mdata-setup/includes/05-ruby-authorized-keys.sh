75204c81477dbcffc05813aed7c5678e01387da6
jfqd <jfqd@blun.org>
2019-11-09 10:16:19 +0100

    take the right user key

--- copy/opt/qutic/var/mdata-setup/includes/05-ruby-authorized-keys.sh
+++ copy/opt/qutic/var/mdata-setup/includes/05-ruby-authorized-keys.sh
@@ -6,8 +6,8 @@ if mdata-get ruby_authorized_keys 1>/dev/null 2>&1; then
   mkdir -p ${home}/.ssh
   echo "# This file is managed by mdata-get ruby_authorized_keys" \
     > ${home}/.ssh/authorized_keys
-  mdata-get admin_authorized_keys >> ${home}/.ssh/authorized_keys
+  mdata-get ruby_authorized_keys >> ${home}/.ssh/authorized_keys
   chmod 700 ${home}/.ssh
-  chmod 644 ${home}/.ssh/authorized_keys
+  chmod 640 ${home}/.ssh/authorized_keys
   chown ruby:ruby ${home}/.ssh/authorized_keys
 fi
