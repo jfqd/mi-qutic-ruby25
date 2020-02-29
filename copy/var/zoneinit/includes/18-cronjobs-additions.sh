a6ddf1783755f0edf1bfc67b37ba5af9838d73b8
jfqd <jfqd@blun.org>
2020-02-29 16:30:04 +0100

    move checks into zabbix_-script

--- copy/var/zoneinit/includes/18-cronjobs-additions.sh
+++ copy/var/zoneinit/includes/18-cronjobs-additions.sh
@@ -5,6 +5,7 @@ cat >> /var/spool/cron/crontabs/root << EOF
 0 3 * * * rm /var/tmp/buffer.* 2>/dev/null || true
 0 4 * * * /usr/bin/find /var/tmp/ -mmin +1440 | /opt/local/bin/grep passenger-error | /usr/bin/xargs /usr/bin/rm
 0,5,10,15,20,25,30,35,40,45,50,55 * * * * /usr/bin/mv /tmp/passenger-error* /var/tmp 2>/dev/null || true
+0,5,10,15,20,25,30,35,40,45,50,55 * * * * /opt/local/bin/zabbix_passenger >/dev/null 2>&1
 EOF
 
 echo '0,10,20,30,40,50 * * * * if [[ `/opt/qutic/bin/psof --count-numbers-only` -gt 5000 ]]; then /usr/sbin/svcadm restart apache; fi' \
